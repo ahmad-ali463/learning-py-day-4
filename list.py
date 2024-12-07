26c679930e5dec8dbe1dd7ed1b20208584adbf51
Ahmad Ali <spyali463@gmail.com>
2024-12-07 21:30:48 +0500

    list && tuples

--- list.py
+++ list.py
@@ -15,10 +15,67 @@
 # how to add reverse in list
 
 
-list = [1,2,4,5]
-print(list.append(3), list)
-print(list.sort(),list)
-print(list.sort(reverse=True),list)
-print(list.reverse(),list)
-print(list.insert(1,6),list)
-print(list.remove(6), list)
+# list = [1,2,4,5]
+# print(list.append(3), list)
+# print(list.sort(),list)
+# print(list.sort(reverse=True),list)
+# print(list.reverse(),list)
+# print(list.insert(1,6),list)
+# print(list.remove(6), list)
+
+
+
+
+# movies = []
+# mov1 = input("Enter 1st movie :")
+# mov2 = input("Enter 2nd movie :")
+# mov3 = input("Enter 3rd movie :")
+# movies.append(mov1)
+# movies.append(mov2)
+# movies.append(mov3)
+
+# print(movies)
+
+
+
+
+# list1 = [5,4,3,2,1,2,3,4,5]
+# list2 = [1,2,2,2,3,4]
+# copy_list1  = list1.copy()
+# copy_list1.reverse()
+
+# if(list1 == copy_list1):
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+
+
+# list = ['D','F','A','C','B','E']
+# list.sort()
+# print(list)
+
+
+
+# numbers_list = [5, 12, 7, 18, 25, 3]
+# numbers_list.sort()
+# print(numbers_list)
+
+
+
+
+
+
+
+# numbers_list = [5, 12, 7, 18, 25, 3]
+# numbers_list.append(13)
+# print(numbers_list)
+
+
+numbers_list = [5, 12, 7, 18, 25, 3]
+numbers_list = sum(numbers_list)
+print(numbers_list)
+
+
+
+    
---
+++ tuple.py
@@ -0,0 +1,35 @@
+# tup = (1 ,1,2,1,1,4,3)
+# print(tup.index(3),tup)
+# print(tup.count(1),tup)
+
+
+
+
+# tuple = ("A", 'B','C','A','D','A')
+# print(tuple.count("A"))
+# print(tuple)
+
+# names_tuple = ("Alice", "Bob", "Charlie", "Diana")
+# names_tuple = list(names_tuple)
+# print(names_tuple)
+
+
+
+
+names_tuple = ("Alice", "Bob", "Charlie", "Diana")
+# print(names_tuple.index("Bob"))
+# print(names_tuple)
+reversed_tuple = names_tuple[::-1]
+print(reversed_tuple)
+
+
+
+
+
+
+
+
+
+
+
+
