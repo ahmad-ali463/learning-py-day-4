48f020938ac4604c462509a819a19e9e49e622d7
Ahmad Ali <spyali463@gmail.com>
2024-12-07 20:21:43 +0500

    list python

---
+++ list.py
@@ -0,0 +1,24 @@
+# marks = ["Ahmad",65,"minha",80 ,"Ali",83]
+# print(type(marks))
+# print(marks)
+# print(len(marks),marks)
+
+
+# student = ["Ahmad" , 17 , 93.3 , "faisalabad"]
+# print(student)
+# student = student[1]
+# print (student)
+
+# how to add append in list
+# how to add sort for ascending order
+# how to add sort for desending order
+# how to add reverse in list
+
+
+list = [1,2,4,5]
+print(list.append(3), list)
+print(list.sort(),list)
+print(list.sort(reverse=True),list)
+print(list.reverse(),list)
+print(list.insert(1,6),list)
+print(list.remove(6), list)
